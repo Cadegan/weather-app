3af2e4449879d233c2b36f55a1207b24f44d36ec
Cadegan <91974649+Cadegan@users.noreply.github.com>
2023-04-09 00:10:25 +0900

    MainWeatherInfo N/A update

--- src/components/MainWeatherInfo.tsx
+++ src/components/MainWeatherInfo.tsx
@@ -17,88 +17,99 @@ const MainWeatherInfo = ({ weatherData }: WeatherInfoProps) => {
 
   return (
     <>
-      {weatherData && (
-        <div className="flex flex-col items-center mb-4 p-4">
-          <div className="flex flex-col items-center">
-            <p className=" text-4xl mb-4 text-shadow-engraved">
-              {weatherData.name}, {weatherData.sys.country}
-            </p>
-            <p className="text-6xl mb-4">
-              {kelvinToCelsius(weatherData.main.temp)}°C
-              {/* {kelvinToFahrenheit(weatherData.main.temp)}°F */}
-            </p>
-            <p className="text-4xl mb-4">
-              {capitalizeFirstLetter(weatherData.weather[0].description)}
-            </p>
-          </div>
-          <div className="flex flex-row max-w-max items-center">
-            <div className="flex flex-col text-right justify-between w-72 h-24">
-              <div className="flex items-center justify-end">
-                <p>{kelvinToCelsius(weatherData.main.feels_like)}°C</p>
-                <img
-                  src={icons.feelsLike}
-                  alt="Feels like icon"
-                  className="h-6 ml-3"
-                />
-              </div>
+      {/* {weatherData && ( */}
+      <div className="flex flex-col items-center mb-4 p-4">
+        <div className="flex flex-col items-center">
+          <p className=" text-4xl mb-4 text-shadow-engraved">
+            {weatherData
+              ? `${weatherData.name}, ${weatherData.sys.country}`
+              : `Localisation`}
+          </p>
+          <p className="text-6xl mb-4">
+            {weatherData
+              ? `${kelvinToCelsius(weatherData.main.temp)}°C`
+              : `Temperature`}
 
-              <div className="flex items-center justify-end">
-                <p>{weatherData.main.humidity}%</p>
-                <img
-                  src={icons.humidity}
-                  alt="Humidity icon"
-                  className="h-6 ml-3"
-                />
-              </div>
-              <div className="flex items-center justify-end">
-                <p>{weatherData.wind.speed} m/s</p>
-                <img
-                  src={icons.windSpeed}
-                  alt="Humidity icon"
-                  className="h-6 ml-3"
-                />
-              </div>
+            {/* {kelvinToFahrenheit(weatherData.main.temp)}°F */}
+          </p>
+          <p className="text-4xl mb-4">
+            {weatherData
+              ? `${capitalizeFirstLetter(weatherData.weather[0].description)}`
+              : `Weather`}
+          </p>
+        </div>
+        <div className="flex flex-row max-w-max items-center">
+          <div className="flex flex-col text-right justify-between w-72 h-24">
+            <div className="flex items-center justify-end">
+              <p>
+                {weatherData
+                  ? ` ${kelvinToCelsius(weatherData.main.feels_like)}°C`
+                  : `N/A`}
+              </p>
+              <img
+                src={icons.feelsLike}
+                alt="Feels like icon"
+                className="h-6 ml-3"
+              />
             </div>
-            <div className="h-20 w-1 shadow-neumorphicLine rounded-lg mx-8"></div>
-            <div className="flex flex-col justify-between w-72 h-24">
-              <div className=" flex items-center">
-                <img
-                  src={icons.pressure}
-                  alt="Pressure icon"
-                  className=" h-6 mr-3"
-                />
-                <p>{weatherData.main.pressure} hPa</p>
-              </div>
-              <div className=" flex items-center">
-                <img
-                  src={icons.sunrise}
-                  alt="Sunrise icon"
-                  className=" h-6 mr-3"
-                />
-                <p>
-                  {convertToLocalTime(
-                    weatherData.sys.sunrise,
-                    weatherData.timezone
-                  )}
-                </p>
-              </div>
-              <div className=" flex items-center">
-                <img
-                  src={icons.sunset}
-                  alt="SunSet icon"
-                  className=" h-6 mr-3"
-                />
-                <p>
-                  {convertToLocalTime(
-                    weatherData.sys.sunset,
-                    weatherData.timezone
-                  )}
-                </p>
-              </div>
+
+            <div className="flex items-center justify-end">
+              <p>{weatherData ? `${weatherData.main.humidity}%` : `N/A`}</p>
+              <img
+                src={icons.humidity}
+                alt="Humidity icon"
+                className="h-6 ml-3"
+              />
+            </div>
+            <div className="flex items-center justify-end">
+              <p>{weatherData ? `${weatherData.wind.speed} m/s` : `N/A`}</p>
+              <img
+                src={icons.windSpeed}
+                alt="Humidity icon"
+                className="h-6 ml-3"
+              />
+            </div>
+          </div>
+          <div className="h-20 w-1 shadow-neumorphicLine rounded-lg mx-8"></div>
+          <div className="flex flex-col justify-between w-72 h-24">
+            <div className=" flex items-center">
+              <img
+                src={icons.pressure}
+                alt="Pressure icon"
+                className=" h-6 mr-3"
+              />
+              <p>{weatherData ? `${weatherData.main.pressure} hPa` : `N/A`}</p>
+            </div>
+            <div className=" flex items-center">
+              <img
+                src={icons.sunrise}
+                alt="Sunrise icon"
+                className=" h-6 mr-3"
+              />
+              <p>
+                {weatherData
+                  ? `${convertToLocalTime(
+                      weatherData.sys.sunrise,
+                      weatherData.timezone
+                    )}`
+                  : `N/A`}
+              </p>
+            </div>
+            <div className=" flex items-center">
+              <img src={icons.sunset} alt="SunSet icon" className=" h-6 mr-3" />
+              <p>
+                {weatherData
+                  ? `${convertToLocalTime(
+                      weatherData.sys.sunset,
+                      weatherData.timezone
+                    )}`
+                  : `N/A`}
+              </p>
             </div>
           </div>
         </div>
-      )}
+      </div>
+      {/* )} */}
     </>
   );
 };
